3def0a6ce2b19330bdc863e106edbf74164ff1c4
CBA <clement.baraise@beconfig.com>
2023-11-14 17:12:32 +0100

    feat(chatbot):test chatgpt (utiliser autre api car payant pour test)

--- apps/libs/library-chatbot/src/lib/part/chatbot/chatbot-test.part.ts
+++ apps/libs/library-chatbot/src/lib/part/chatbot/chatbot-test.part.ts
@@ -1,70 +1,98 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'chat-bot-test',
   templateUrl: './chatbot-test.part.html',
   styleUrls: ['./chatbot-test.part.scss'],
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class ChatBotTestPart {
-  
-  displayBot(){
-    const chat = document.getElementById("chat") as HTMLInputElement ;
-    chat.style.display="block";
-    const displayBot = document.getElementById("displayBot") as HTMLInputElement ;
-    displayBot.style.display="none";
+  static API_KEY = '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';
+  displayBot() {
+    const chat = document.getElementById('chat') as HTMLInputElement;
+    chat.style.display = 'block';
+    const displayBot = document.getElementById(
+      'displayBot'
+    ) as HTMLInputElement;
+    displayBot.style.display = 'none';
   }
-  
-  closeChat(){
-    const chat = document.getElementById("chat") as HTMLInputElement ;
-    chat.style.display="none";
-    const displayBot = document.getElementById("displayBot") as HTMLInputElement ;
-    displayBot.style.display="block";
-    const notif = document.getElementById("notif") as HTMLInputElement ;
-    notif.style.display="none";
 
-   
+  closeChat() {
+    const chat = document.getElementById('chat') as HTMLInputElement;
+    chat.style.display = 'none';
+    const displayBot = document.getElementById(
+      'displayBot'
+    ) as HTMLInputElement;
+    displayBot.style.display = 'block';
+    const notif = document.getElementById('notif') as HTMLInputElement;
+    notif.style.display = 'none';
   }
-  messageSubmit(){
-    const message = document.getElementById("messageContainer")as HTMLInputElement;
+  messageSubmit() {
+    const message = document.getElementById(
+      'messageContainer'
+    ) as HTMLInputElement;
     const texte = message.value.trim();
-    message.value='';
+    message.value = '';
+
     if (texte !== '') {
-
       const divMessageUtilisateur = document.createElement('div');
-
-      
       divMessageUtilisateur.className = 'messages-user';
       divMessageUtilisateur.textContent = texte;
-      divMessageUtilisateur.setAttribute("style" , "padding: 0.5rem 2rem 0.7rem;border-radius: 10px 10px 10px 0;background:  #3764ed;margin-top: 1rem;font-size: medium;position: absolute;right: 5%;color: #fff;font-family: Arial, Helvetica, sans-serif;width: auto;max-width: 70%;box-sizing: border-box;");
+      divMessageUtilisateur.setAttribute(
+        'style',
+        'padding: 0.7rem 0.5rem 0.5rem;border-radius: 10px 10px 0 10px;background: #3764ed;margin-bottom: 1rem;position: relative;text-align: right;right: -72%;color: #fff;font-family: Arial, Helvetica, sans-serif;font-size: medium;width: auto;max-width: 70%;box-sizing: border-box;overflow: hidden;word-wrap: break-word;width:20%'
+      );
 
-      const contenuMessages = document.querySelector('.discussion')as HTMLInputElement;
-      contenuMessages.style.visibility="visible";
-      
+      const contenuMessages = document.querySelector(
+        '.discussion'
+      ) as HTMLInputElement;
+      contenuMessages.style.visibility = 'visible';
+
       contenuMessages.appendChild(divMessageUtilisateur);
-      const typingBot=document.getElementById('typing-loader') as HTMLInputElement;
-      this.chatBot(texte);
-      typingBot.style.visibility="visible"
-      setTimeout(() => {
-        typingBot.style.visibility="hidden"
-      }, 10000);
- 
+      const typingBot = document.getElementById(
+        'typing-loader'
+      ) as HTMLInputElement;
+      typingBot.style.visibility = 'visible';
+      this.generateReponse(texte).then(data=>{
+        this.chatBot(data);
+      })
+
+      // setTimeout(() => {
+      //   this.chatBot(texte);
+      //   typingBot.style.visibility = 'hidden';
+      // }, 3000);
     }
   }
-  chatBot(texte:string){
-    if(texte == "bonjour" || texte == "Bonjour" || texte == "salut" || texte== "Salut"){
-      const botMessage = document.createElement('div');
-      botMessage.className = "messages";
-      botMessage.setAttribute("style" , "padding: 0.5rem 2rem 0.7rem;border-radius: 10px 10px 10px 0;background: rgba(0, 0, 0, 0.3);margin-top: 1rem;font-size: medium;position: relative;left: 5%;color: #fff;font-family: Arial, Helvetica, sans-serif;width: auto;max-width: 70%;box-sizing: border-box;");
-      botMessage.textContent = "Bonjour , comment allez-vous ?";
-      const contenuMessages = document.querySelector('.discussion')as HTMLElement;
-      contenuMessages.appendChild(botMessage);
 
-    }
+  chatBot(texte: string) {
+    const botMessage = document.createElement('div');
+    botMessage.className = 'messages';
+    botMessage.setAttribute(
+      'style',
+      'display: block;padding: 0.5rem 2rem 0.7rem;border-radius: 10px 10px 10px 0;background: rgba(0, 0, 0, 0.3);margin-top: 1rem;font-size: medium;position: relative;left: 5%;color: #fff;font-family: Arial, Helvetica, sans-serif;width: auto;max-width: 70%;box-sizing: border-box;'
+    );
 
+    const contenuMessages = document.querySelector(
+      '.discussion'
+    ) as HTMLElement;
+    contenuMessages.appendChild(botMessage);
   }
-  
 
-  
+  public generateReponse(texte: string): Promise<string> {
+    const API_URL = 'https://api.openai.com/v1/chat/completions';
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${ChatBotTestPart.API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: texte }],
+      }),
+    };
+
+    return fetch(API_URL, requestOptions).then(res=>res.json());
+  }
 }
